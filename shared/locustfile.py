b816cedb1d10ee1d7ca61718a0a9809be49dae19
Clemens Wolff <clewolff@microsoft.com>
2017-06-14 00:51:32 +0200

    Add basic load test script

---
+++ shared/locustfile.py
@@ -0,0 +1,18 @@
+from random import randint
+
+from locust import HttpLocust
+from locust import TaskSet
+from locust import task
+
+
+class WebsiteTasks(TaskSet):
+    @task
+    def test(self):
+        ident = randint(1, 1000000)
+        self.client.get('/test/%s' % ident)
+
+
+class WebsiteUser(HttpLocust):
+    task_set = WebsiteTasks
+    min_wait = 500
+    max_wait = 2000
